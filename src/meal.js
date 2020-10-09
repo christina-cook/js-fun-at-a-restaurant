4c8f9505b907e884c5f24ccb7ac337a64838adbd
Christina Cook <cmc.cook7@gmail.com>
2020-10-08 20:46:27 -0600

    Pass Meal Tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,46 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  return  `Delicious ${food}`;
 }
 
+
+function createMenuItem(menuItem, itemCost, typeOfMeal) {
+  return {
+    name: menuItem,
+    price: itemCost,
+    type: typeOfMeal
+  }
+}
+
+function addIngredients(newIngredient, ingredients) {
+  if (ingredients.includes(newIngredient) === false) {
+    ingredients.push(newIngredient);
+  }
+  return ingredients;
+}
+
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+}
+
+function decreasePrice(initialPrice) {
+  var discount = initialPrice / 10;
+  return initialPrice - discount;
+}
+
+function createRecipe(menuItem, ingredients, typeOfMeal) {
+  return {
+    title: menuItem,
+    ingredients: ingredients,
+    type: typeOfMeal
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
